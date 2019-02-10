73cc4c54c2809a5c467fe6a470fc1bb9e193f18c
Leevi <46795390+leevilaitala@users.noreply.github.com>
2019-02-10 21:43:55 +0200

    Create configure_monitors.sh

---
+++ scripts/configure_monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --primary --mode 3840x2160 --pos 1920x0 --rotate normal --output DVI-D-0 --mode 1920x1200 --pos 0x160 --rotate normal --output DVI-D-1 --off --output HDMI-A-0 --off
